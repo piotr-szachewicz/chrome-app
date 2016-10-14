e41bf0df3993b2bb945cb25f864db32eb38f9808
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2016-10-14 15:21:47 +0200

    Added a click sound, when clicking on the Start button.

--- metronome.js
+++ metronome.js
@@ -3,9 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
   var stopButton = document.querySelector('#stop');
   var bpmField = document.querySelector('#bpm');
 
+  var clickSound = new Audio('click.mp3');
+
   startButton.addEventListener('click', function() {
     console.log("started!");
     console.log("bpm value is " + bpmField.value);
+    clickSound.play();
   });
 
   stopButton.addEventListener('click', function() {
