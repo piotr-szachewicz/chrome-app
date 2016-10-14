3df541c8a0baf34a0e2aa53da93a1a1e0d14a617
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2016-10-14 15:40:45 +0200

    First version of a working metronome.

--- metronome.js
+++ metronome.js
@@ -1,18 +1,24 @@
+var clickSound = new Audio('click.mp3');
+var interval;
+
+function playClick() {
+  clickSound.play();
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var startButton = document.querySelector('#start');
   var stopButton = document.querySelector('#stop');
   var bpmField = document.querySelector('#bpm');
 
-  var clickSound = new Audio('click.mp3');
-
   startButton.addEventListener('click', function() {
-    console.log("started!");
-    console.log("bpm value is " + bpmField.value);
-    clickSound.play();
+    var msBetweenClicks = 60000/parseInt(bpmField.value);
+
+    clearInterval(interval);
+    interval = setInterval(playClick, msBetweenClicks);
   });
 
   stopButton.addEventListener('click', function() {
-    console.log("stopped!");
+    clearInterval(interval);
   });
 });
 
