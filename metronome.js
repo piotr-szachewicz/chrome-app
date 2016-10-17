381fadea77be3b41ef8f623220fbc890793b9df4
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2016-10-17 23:23:39 +0200

    Added a tap tempo functionality to the program.
    Click on the background and then tap the tempo
    using some keyboard key.

--- metronome.js
+++ metronome.js
@@ -5,6 +5,8 @@ worker.addEventListener('message', function(e) {
   clickSound.play();
 });
 
+var tapTempoTimestamps = [];
+
 document.addEventListener('DOMContentLoaded', function() {
   var startButton = document.querySelector('#start');
   var stopButton = document.querySelector('#stop');
@@ -20,3 +22,35 @@ document.addEventListener('DOMContentLoaded', function() {
     worker.postMessage({type: 'stop'});
   });
 });
+
+function calculateBpmFromTapTempo() {
+  var sumOfDifferences = 0;
+  for (var i = 1; i < tapTempoTimestamps.length; i++) {
+    sumOfDifferences += (tapTempoTimestamps[i] - tapTempoTimestamps[i-1]);
+  }
+
+  var averageDiff = sumOfDifferences / (tapTempoTimestamps.length-1);
+  return Math.round(60000 / averageDiff);
+}
+
+document.onkeypress = function(e) {
+  var tagName = e.target.tagName;
+  if (tagName != "INPUT" && tagName != "BUTTON") {
+    var currentTimestamp = Date.now();
+
+    if (tapTempoTimestamps.length > 0) {
+      var timeSinceLastTap = currentTimestamp - tapTempoTimestamps[tapTempoTimestamps.length-1];
+      if (timeSinceLastTap > 4000) {
+        tapTempoTimestamps = [];
+      }
+    }
+
+    tapTempoTimestamps.push(currentTimestamp);
+
+    if (tapTempoTimestamps.length > 1) {
+      var bpm = calculateBpmFromTapTempo();
+      var bpmField = document.querySelector('#bpm');
+      bpmField.value = bpm;
+    }
+  }
+}
